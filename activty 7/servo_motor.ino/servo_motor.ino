ca02053d4a5d5dba53b81b3a0cf9c846151f3372
Sofia-Lorraine <2300541@ub.edu.ph>
2026-03-05 15:57:40 +0800

    Add files via upload

---
+++ activty 7/servo_motor.ino/servo_motor.ino
@@ -0,0 +1,35 @@
+#include <Servo.h>
+
+const int SERVO_PIN = 9;
+Servo myServo;
+String readString;
+int currentServoAngle = 90;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  myServo.attach(SERVO_PIN);
+  myServo.write(currentServoAngle);
+  Serial.println("SYSTEM_READY");
+}
+
+void loop() {
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c == '\n') {
+      readString.trim();
+      if (readString.length() > 0) {
+        int desiredAngle = readString.toInt();
+        if (desiredAngle >= 0 && desiredAngle <= 180) {
+          myServo.write(desiredAngle);
+          Serial.print("OK:"); Serial.println(desiredAngle);
+        } else {
+          Serial.println("ERROR:OUT_OF_RANGE");
+        }
+      }
+      readString = "";
+    } else if (c != '\r') {
+      readString += c;
+    }
+  }
+}
